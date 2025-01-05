df1f8d121a08c377a27ce573202ec9b79f9fab3d
creater-tk <tharunkumarboddeti@gmail.com>
2025-01-05 11:09:13 +0530

    updated

--- payment-function/payment.action.js
+++ payment-function/payment.action.js
@@ -1,23 +1,5 @@
-import { initializePayment } from "@/lib/appwrite";
-// import { NextApiRequest, NextApiResponse } from "next";
+export async function main(req, res) {
+  console.log("Function triggered!");
 
-export const handlePayment = async (req , res)=>{
-  try {
-    if(req.method === "POST"){
-      const {amount, currency} = req.body;
-
-      if (!amount) {
-        return res.status(400).json({ message: "Amount is required" });
-      }
-
-      const payment = await initializePayment(amount,currency);
-
-      res.status(200).json(payment);
-    }else{
-      res.status(405).json({message: "Method not allowed"});
-    }
-  } catch (error) {
-    res.status(500).json({message: "Internal server error"});
-    console.log("Error:",error.message)
-  }
+  return res.json({ message: "Function executed successfully!" }, 200);
 }
