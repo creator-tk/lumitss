cb16d523d4cbdf6254c39ad3a2a8b374431eab69
Tharun Kumar Boddeti <112042715+creator-tk@users.noreply.github.com>
2025-01-06 05:48:30 +0530

    Update payment.action.js

--- payment-function/payment.action.js
+++ payment-function/payment.action.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function main({req, res}) {
-  console.log("Function triggered!");
+export async function main({req, res, log}) {
+  log("Function triggered!");
 
   return res.json({ message: "Function executed successfully!" }, 200);
 }
