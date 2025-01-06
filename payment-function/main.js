092206f8d886627ad830675e940488271d78f305
Tharun Kumar Boddeti <112042715+creator-tk@users.noreply.github.com>
2025-01-06 07:28:49 +0530

    Update main.js

--- payment-function/main.js
+++ payment-function/main.js
@@ -3,13 +3,9 @@ import Razorpay from "razorpay";
 export default async function main({ req, res, log }) {
   log("Function triggered!");
 
-  // Log API keys (DO NOT log your secret key in production)
-  log("Razorpay Key ID:", process.env.RAZORPAY_KEY_ID);
-  log("Razorpay Key Secret:", process.env.RAZORPAY_KEY_SECRET);
-
   const razorpay = new Razorpay({
-    key_id:"rzp_test_DCEhb7USpZ3Chq",
-    key_secret:"05xtdn20ftv8ANiAEK5gEYHf",
+    key_id:process.env.RAZORPAY_KEY_ID,
+    key_secret:process.env.RAZORPAY_KEY_SECRET,
   });
 
   const { currency = "INR", amount } = req.body;
