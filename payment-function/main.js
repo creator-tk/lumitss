f63d3afe6e0089023c6a1e2885fca51ee2ad105a
Tharun Kumar Boddeti <112042715+creator-tk@users.noreply.github.com>
2025-01-06 06:02:55 +0530

    Update main.js

--- payment-function/main.js
+++ payment-function/main.js
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
  export default async function main({ req, res, log }) {
   log("Function triggered!");
-  return res.json({ message: "Function executed successfully!" }, 200);
+  const {currency, amount} = req.body;
+  return res.json({ message: "Function executed successfully!", Currency: `${currency}` }, 200);
 };
