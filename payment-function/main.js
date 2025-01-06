2d538db496f08f193dc68814e6a398be292cee5b
creater-tk <tharunkumarboddeti@gmail.com>
2025-01-06 06:30:11 +0530

    payment function updated

--- payment-function/main.js
+++ payment-function/main.js
@@ -1,18 +1,30 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import Razorpay from "razorpay";
+
 export default async function main({ req, res, log }) {
   log("Function triggered!");
 
+  const razorpay = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET
+  });
+
   const body = JSON.parse(req.body || "{}");
   const { currency, amount } = body;
-  log("currency:" , currency, "amount" , amount);
-  // Validate if required data exists
+
   if (!currency || !amount) {
     return res.json({
       error: "Currency and amount are required",
     });
   }
 
-  // Return the proper response
+  const options = {
+    amount: amount*100,
+    currency: currency || "INR",
+    receipt: `receipt_${Date.now()}`
+  }
+
+  const order = await razorpay.orders.create(options);
+  res.send(order);
   return res.json({
     message: "Function executed successfully!",
     Currency: currency,
