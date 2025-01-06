3372e50a158c5f27ea030e842ff18a5269a13be1
creater-tk <tharunkumarboddeti@gmail.com>
2025-01-06 05:55:39 +0530

    updated

--- payment-function/main.js
+++ payment-function/main.js
@@ -1,6 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function main({req, res, log}) {
+/* eslint-disable @typescript-eslint/no-unused-vars */
+module.exports = async function main({ req, res, log }) {
   log("Function triggered!");
-
   return res.json({ message: "Function executed successfully!" }, 200);
-}
+};
