280216d27f07892250347b8ee75ec5421372f8f9
Tharun Kumar Boddeti <112042715+creator-tk@users.noreply.github.com>
2025-01-06 06:36:15 +0530

    Update main.js

--- payment-function/main.js
+++ payment-function/main.js
@@ -11,6 +11,8 @@ export default async function main({ req, res, log }) {
   const body = JSON.parse(req.body || "{}");
   const { currency, amount } = body;
 
+  log("currency:" , currency, "Ammount":amount);
+
   if (!currency || !amount) {
     return res.json({
       error: "Currency and amount are required",
