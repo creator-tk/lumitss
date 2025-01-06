57a6a82c09229f9f5a5d331e135c6a695ce8976d
Tharun Kumar Boddeti <112042715+creator-tk@users.noreply.github.com>
2025-01-06 06:09:37 +0530

    Update main.js

--- payment-function/main.js
+++ payment-function/main.js
@@ -1,6 +1,21 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
- export default async function main({ req, res, log }) {
+export default async function main({ req, res, log }) {
   log("Function triggered!");
-  const {currency, amount} = req.body;
-  return res.json({ message: "Function executed successfully!", Currency: currency }, 200);
-};
+
+  const { currency, amount } = req.body;
+
+  // Validate if required data exists
+  if (!currency || !amount) {
+    return res.json({
+      error: "Currency and amount are required",
+    });
+  }
+
+  // Return the proper response
+  return res.json({
+    message: "Function executed successfully!",
+    Currency: currency,
+    Amount: amount,
+  });
+}
+
