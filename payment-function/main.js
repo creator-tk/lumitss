18cf2bbfe2125823ba89847fe2c3678024b24c92
Tharun Kumar Boddeti <112042715+creator-tk@users.noreply.github.com>
2025-01-06 06:04:58 +0530

    Update main.js

--- payment-function/main.js
+++ payment-function/main.js
@@ -2,5 +2,5 @@
  export default async function main({ req, res, log }) {
   log("Function triggered!");
   const {currency, amount} = req.body;
-  return res.json({ message: "Function executed successfully!", Currency: `${currency}` }, 200);
+  return res.json({ message: "Function executed successfully!", Currency: currency }, 200);
 };
