d1554c434fa46ca8dabe0cfe55c3ede364b63e49
creater-tk <tharunkumarboddeti@gmail.com>
2025-01-10 13:09:58 +0530

    Title changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "AmazingCart",
+  title: "LUMITSS",
   description: "Get all you nessarys at lowest prices",
 };
 
