6e01bd2fbc94c0839d5b87ddd307c6d7a4bba0a6
creater-tk <tharunkumarboddeti@gmail.com>
2025-01-10 16:00:55 +0530

    merge comments removed

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images:{
     domains: ["i.etsystatic.com", "cloud.appwrite.io"]
-  },
-<<<<<<< HEAD
-=======
-  // output: "export",
->>>>>>> 4b53112c7b5fd96c632f6595458d68abcaf0cb31
-
+  }
 };
 
 export default nextConfig;
